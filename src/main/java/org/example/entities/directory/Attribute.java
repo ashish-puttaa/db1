b71f4fe483681f57a9a2042113a1a906e2cba00f
Ashish Puttaa <ashishputtaa.98@gmail.com>
2023-10-12 20:37:37 +0530

    add static deserialize method to Attribute interface

--- src/main/java/org/example/entities/directory/Attribute.java
+++ src/main/java/org/example/entities/directory/Attribute.java
@@ -4,4 +4,10 @@ public interface Attribute<T> {
     T getValue();
     AttributeType getType();
     byte[] serialize();
+
+    static Attribute<?> deserialize(byte[] bytes) {
+        // Implementing classes must provide their own deserialization logic
+        // This is just a placeholder, you may need to modify it based on your requirements
+        throw new UnsupportedOperationException("Deserialization not implemented");
+    }
 }
