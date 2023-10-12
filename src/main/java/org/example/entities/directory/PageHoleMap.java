df730124cd7196d58093512117741ebde8f320ee
Ashish Puttaa <ashishputtaa.98@gmail.com>
2023-10-13 03:09:22 +0530

    move the holes map into a separate class

---
+++ src/main/java/org/example/entities/directory/PageHoleMap.java
@@ -0,0 +1,66 @@
+package org.example.entities.directory;
+
+import java.util.*;
+
+public class PageHoleMap {
+    SortedMap<Short, List<Short>> holes;
+
+    public PageHoleMap(PageSlotArrayEntry[] slotArray) {
+        this.holes = this.constructPageHolesMap(slotArray);
+    }
+
+    public Optional<Short> getPageHole(short desiredLength) {
+        Iterator<Map.Entry<Short, List<Short>>> iterator = this.holes.entrySet().iterator();
+
+        while(iterator.hasNext()) {
+            Map.Entry<Short, List<Short>> entry = iterator.next();
+            Short length = entry.getKey();
+
+            if (length >= desiredLength) {
+                List<Short> offsetList = entry.getValue();
+
+                if(!offsetList.isEmpty()) {
+                    short offset = offsetList.remove(0);
+
+                    if(offsetList.isEmpty()) {
+                        iterator.remove();
+                    }
+
+                    return Optional.of(offset);
+                }
+            }
+        }
+
+        return Optional.empty();
+    }
+
+    private SortedMap<Short, List<Short>> constructPageHolesMap(PageSlotArrayEntry[] slotArray) {
+        SortedMap<Short, List<Short>> holesMap = new TreeMap<>();
+
+        PageSlotArrayEntry[] nonEmptySlotsArray = Arrays.stream(slotArray).filter(entry -> !entry.isEmpty()).toArray(PageSlotArrayEntry[]::new);
+        PageSlotArrayEntry[] sortedSlotArray = this.sortSlotArrayByOffset(nonEmptySlotsArray);
+
+        for(int i=1; i<sortedSlotArray.length; i++) {
+            PageSlotArrayEntry previousEntry = sortedSlotArray[i-1];
+            PageSlotArrayEntry currentEntry = sortedSlotArray[i];
+
+            short previousEntryOffsetEnd = (short) (previousEntry.pageOffset + previousEntry.tupleLength - 1);
+            boolean hasGap = currentEntry.pageOffset > previousEntryOffsetEnd;
+
+            if(hasGap) {
+                short holeStartOffset = (short) (previousEntryOffsetEnd + 1);
+                short holeLength = (short) (currentEntry.pageOffset - holeStartOffset);
+
+                holesMap.computeIfAbsent(holeLength, k -> new ArrayList<>()).add(holeStartOffset);
+            }
+        }
+
+        return holesMap;
+    }
+
+    private PageSlotArrayEntry[] sortSlotArrayByOffset(PageSlotArrayEntry[] slotArray) {
+        return Arrays.stream(slotArray)
+                .sorted(Comparator.comparingInt(entry -> entry.pageOffset))
+                .toArray(PageSlotArrayEntry[]::new);
+    }
+}
--- src/main/java/org/example/entities/directory/PageSlotArray.java
+++ src/main/java/org/example/entities/directory/PageSlotArray.java
@@ -9,11 +9,11 @@ public class PageSlotArray {
     List<PageSlotArrayEntry> slotArray;
 
     //METADATA
-    SortedMap<Short, List<Short>> pageHolesMap;
+   PageHoleMap pageHolesMap;
 
     public PageSlotArray(PageSlotArrayEntry[] slotArray) {
         this.slotArray = List.of(slotArray);
-        this.pageHolesMap = constructPageHolesMap(slotArray);
+        this.pageHolesMap = new PageHoleMap(slotArray);
     }
 
     public byte[] serialize() {
@@ -61,61 +61,6 @@ public class PageSlotArray {
         return new PageSlotArray(entryList.toArray(PageSlotArrayEntry[]::new));
     }
 
-    private PageSlotArrayEntry[] sortSlotArrayByOffset(PageSlotArrayEntry[] slotArray) {
-        return Arrays.stream(slotArray)
-                .sorted(Comparator.comparingInt(entry -> entry.pageOffset))
-                .toArray(PageSlotArrayEntry[]::new);
-    }
-
-    private SortedMap<Short, List<Short>> constructPageHolesMap(PageSlotArrayEntry[] slotArray) {
-        SortedMap<Short, List<Short>> holesMap = new TreeMap<>();
-
-        PageSlotArrayEntry[] nonEmptySlotsArray = Arrays.stream(slotArray).filter(entry -> !entry.isEmpty()).toArray(PageSlotArrayEntry[]::new);
-        PageSlotArrayEntry[] sortedSlotArray = sortSlotArrayByOffset(nonEmptySlotsArray);
-
-        for(int i=1; i<sortedSlotArray.length; i++) {
-            PageSlotArrayEntry previousEntry = sortedSlotArray[i-1];
-            PageSlotArrayEntry currentEntry = sortedSlotArray[i];
-
-            short previousEntryOffsetEnd = (short) (previousEntry.pageOffset + previousEntry.tupleLength - 1);
-            boolean hasGap = currentEntry.pageOffset > previousEntryOffsetEnd;
-
-            if(hasGap) {
-                short holeStartOffset = (short) (previousEntryOffsetEnd + 1);
-                short holeLength = (short) (currentEntry.pageOffset - holeStartOffset);
-
-                holesMap.computeIfAbsent(holeLength, k -> new ArrayList<>()).add(holeStartOffset);
-            }
-        }
-
-        return holesMap;
-    }
-
-    public Optional<Short> getPageHole(short desiredLength) {
-        Iterator<Map.Entry<Short, List<Short>>> iterator = this.pageHolesMap.entrySet().iterator();
-
-        while(iterator.hasNext()) {
-            Map.Entry<Short, List<Short>> entry = iterator.next();
-            Short length = entry.getKey();
-
-            if (length >= desiredLength) {
-                List<Short> offsetList = entry.getValue();
-
-                if(!offsetList.isEmpty()) {
-                    short offset = offsetList.remove(0);
-
-                    if(offsetList.isEmpty()) {
-                        iterator.remove();
-                    }
-
-                    return Optional.of(offset);
-                }
-            }
-        }
-
-        return Optional.empty();
-    }
-
     public int insertSlot(short pageOffset, short tupleLength) {
         for(int i=0; i<this.slotArray.size(); i++) {
             PageSlotArrayEntry entry = this.slotArray.get(i);
@@ -140,7 +85,7 @@ public class PageSlotArray {
             short holeStartOffset = entryToRemove.pageOffset;
             short holeLength = entryToRemove.tupleLength;
 
-            this.pageHolesMap.computeIfAbsent(holeLength, k -> new ArrayList<>()).add(holeStartOffset);
+            this.pageHolesMap.holes.computeIfAbsent(holeLength, k -> new ArrayList<>()).add(holeStartOffset);
         }
     }
 }
