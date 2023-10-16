6298dff7f4da5c42e98094635f5942f53427b0e5
Ashish Puttaa <ashishputtaa.98@gmail.com>
2023-10-17 02:15:27 +0530

    simplify buffer lru cache and add read write locks

--- src/main/java/org/example/entities/memory/BufferLRUCache.java
+++ src/main/java/org/example/entities/memory/BufferLRUCache.java
@@ -1,41 +1,47 @@
 package org.example.entities.memory;
 
-import java.util.HashMap;
-import java.util.LinkedList;
+import java.util.LinkedHashMap;
+import java.util.Map;
 import java.util.Optional;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 
-// TODO: Make this thread safe
 public class BufferLRUCache<K, V> {
-    private final LinkedList<K> order;
-    private final HashMap<K, V> cache;
+    private final LinkedHashMap<K, V> cache;
     private final int capacity;
+    private final ReentrantReadWriteLock lock;
 
     public BufferLRUCache(int capacity) {
-        this.order = new LinkedList<>();
-        this.cache = new HashMap<>();
+        this.cache = new LinkedHashMap<>(capacity, 1, true);
         this.capacity = capacity;
+        this.lock = new ReentrantReadWriteLock(true);
     }
 
     public Optional<V> get(K key) {
-        if(this.cache.containsKey(key)) {
-            this.order.remove(key);
-            this.order.addFirst(key);
-            return Optional.of(this.cache.get(key));
+        this.lock.readLock().lock();
+        try {
+            return Optional.ofNullable(this.cache.get(key));
         }
-
-        return Optional.empty();
+        finally {
+            this.lock.readLock().unlock();
+        }
     }
 
     public void put(K key, V value) {
-        if(this.cache.containsKey(key)) {
-            this.order.remove(key);
-        }
-        else if(this.order.size() == this.capacity) {
-            K removedKey = this.order.removeLast();
-            this.cache.remove(removedKey);
-        }
+        this.lock.writeLock().lock();
 
-        this.order.addFirst(key);
-        this.cache.put(key, value);
+        try {
+            if(this.cache.size() >= this.capacity) {
+                this.evict();
+            }
+            this.cache.put(key, value);
+        }
+        finally {
+            this.lock.writeLock().unlock();
+        }
+    }
+
+    private void evict() {
+        Map.Entry<K, V> eldest = this.cache.entrySet().iterator().next();
+        this.cache.remove(eldest.getKey());
     }
 }
